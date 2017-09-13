24d38c8d0b5d7b62e44c4a20165ef0bc94dfff19
michael <mmansourati@gmail.com>
2017-09-12 21:27:01 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+import '../styles/index.scss'
+
+
+const IndexPage = () => (
+  <div className="mainContainer">
+    <div className="splash">
+      <img className="topImage" src="https://i.imgur.com/25o7wqP.jpg"/>
+      <p className="downArrow">⌄</p>
+    </div>
+    <div className="textArea">
+      <h1 id="title">ear camera</h1>
+      <p className="lazyAss">site forthcoming</p>
+    </div>
+  </div>
+)
+
+export default IndexPage
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+require("typeface-raleway")
+
+const typography = new Typography({
+  headerFontFamily: ['raleway', 'sans-serif'],
+  bodyFontFamily: ['raleway', 'sans-serif'],
+  backgroundColor: '#000000',
+})
+
+export default typography
