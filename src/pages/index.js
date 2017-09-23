34ba8bd1890ba13cf3f7ce66c1249cbda06aec99
michael <mmansourati@gmail.com>
2017-09-23 11:56:25 -0400

    removed layout style. interfered with landing page only existing at /

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,36 @@ import '../styles/index.scss'
 
 
 const IndexPage = () => (
-  <div className="page-goes-here">
-    <div className="textArea">
-      <h3>Lorem ipsum</h3>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
+  <div className="mainContainer">
+    <div className="splash">
+      <p className="downArrow">⌄</p>
     </div>
-    <div className="exampleImage">
-      <img className="theBand" src="https://i.imgur.com/Hn4qTke.jpg"/>
+    <div className="contentArea">
+      <h1 id="title">ear camera</h1>
+      <div className="navigation">
+        <Link className="link" to="/about/">about</Link>
+        <Link className="link" to="/media/">media</Link>
+        <Link className="link" to="/personnel">personnel</Link>
+        <Link className="link" to="/events/">events</Link>
+        <Link classNAme="lnik" to="/another-thing/">another thing</Link>
+      </div>
+      <div className="page-goes-here">
+        <div className="textArea">
+          <h3>Lorem ipsum</h3>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </div>
+        <div className="exampleImage">
+          <img className="theBand" src="https://i.imgur.com/Hn4qTke.jpg"/>
+        </div>
+      </div>
     </div>
   </div>
 )
