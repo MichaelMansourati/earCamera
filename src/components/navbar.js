3dde43684f59e53e7c8dfc4cdb4d83e1d7d79d89
michael <mmansourati@gmail.com>
2017-09-25 22:52:30 -0400

    fixed background image displaying on other pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import '../styles/index.scss'
+import '../styles/navbar.scss'
 
 const Navbar = () => (
   <div className="navigation">
--- src/pages/media.js
+++ src/pages/media.js
@@ -5,14 +5,14 @@ import Navbar from '../components/navbar.js'
 
 import '../styles/events.scss'
 
-const events = () => (
+const media = () => (
   <div className="mainContainer">
     <div className="contentArea">
       <h1 id="title">ear camera</h1>
       <Navbar/>
-      <p>you are at the events page</p>
+      <p>you are at the media page</p>
     </div>
   </div>
 )
 
-export default events
+export default media
