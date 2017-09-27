48c2eca4704fd685305c3e349b997103f880692e
michael <mmansourati@gmail.com>
2017-09-26 21:33:55 -0400

    added placeholder photo to media page. replaced lost changes from previous merge

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ import '../styles/navbar.scss'
 const Navbar = () => (
   <div className="navigation">
     <Link className="link" to="/about/">about</Link>
+    <Link className="link" to="/events/">events</Link>
     <Link className="link" to="/media/">media</Link>
     <Link className="link" to="/personnel">personnel</Link>
-    <Link className="link" to="/events/">events</Link>
     <Link className="link" to="/another-thing/">another thing</Link>
   </div>
 )
--- src/pages/media.js
+++ src/pages/media.js
@@ -3,14 +3,21 @@ import Link from 'gatsby-link'
 
 import Navbar from '../components/navbar.js'
 
-import '../styles/events.scss'
+import legg from '../images/legg.jpg'
+
+import '../styles/media.scss'
 
 const media = () => (
   <div className="mainContainer">
     <div className="contentArea">
       <h1 id="title">ear camera</h1>
       <Navbar/>
-      <p>you are at the media page</p>
+      <div className="page-goes-here">
+        <div className="page-goes-here">
+          <p>media forthcoming</p>
+          <img className="legg" src={legg} width="500px"/>
+        </div>
+      </div>
     </div>
   </div>
 )
