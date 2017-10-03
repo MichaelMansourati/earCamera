fe1f62caff5e0cebcb64d0893584015e11733400
michael <mmansourati@gmail.com>
2017-10-02 23:36:11 -0400

    completed productionBuild

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,6 @@ import ensemble from '../images/ensemble.jpg'
 
 const IndexPage = () => (
   <div className="mainContainer">
-    <div className="mobileSplash">
-      <img className="legg" src={legg}/>
-    </div>
     <div className="splash">
       <p className="downArrow">⌄</p>
     </div>
@@ -22,10 +19,43 @@ const IndexPage = () => (
       <h1 id="title">ear camera</h1>
       <Navbar/>
       <div className="page-goes-here">
-        <div className="imgs">
-          <img className="description" src={transcripts}/>
-          <img className="theBand" src={ensemble}/>
+        <div className="textArea">
+          <p>
+            Ear Camera is a collective of bodies creating cochlear art. We are a capillary community, a neural network, an ear that listens, a camerata of peers.
+            Our concerts are a living snapshot of our inner chambers developed in dark rooms and focused onto vibrating membranes. We are a lens that reflects the society in which we live.
+            we believe that all human experience, rendered in photorealistic detail, is worthy of amplification.
+          </p>
+          <p>
+            <span>Ear Camera is: </span>
+            <span>Tommy Davis</span>
+            <span> // </span>
+            <span>Suzu Enns</span>
+            <span> // </span>
+            <a href="https://www.indiayeshe.com">India Gailey</a>
+            <span> // </span>
+            <a href="https://www.jglasermusic.com">Joseph Glaser</a>
+            <span> // </span>
+            <span>Noa Haran</span>
+            <span> // </span>
+            <a href="https://soundcloud.com/justin-jaramillo-7">Justin Jaramillo</a>
+            <span> // </span>
+            <a href="https://soundcloud.com/michaelkimsheng">Michael Kim-Sheng</a>
+            <span> // </span>
+            <a href="http://iwastherebyaccident.tumblr.com/">Francis Leduc-Bélanger</a>
+            <span> // </span>
+            <a href="https://michaelmansourati.com">Michael Mansourati</a>
+            <span> // </span>
+            <span>Chris Paul</span>
+            <span> // </span>
+            <a href="https://www.jamesrubinomusic.com">James Rubino</a>
+            <span> // </span>
+            <a href="https://www.soundcloud.com/keithstratton">Keith Stratton</a>
+          </p>
+          <p>
+            Follow ear camera on <a href="https://www.instagram.com/ear_camera">Instagram</a>.
+          </p>
         </div>
+        <iframe className="youtube" width="560" height="315" src="https://www.youtube.com/embed/FhVnc3K7zo0" frameborder="0" allowfullscreen></iframe>
       </div>
     </div>
   </div>
