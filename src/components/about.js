00c1bbe15383515c7ead8fb63c211ed97830cfe4
michael <mmansourati@gmail.com>
2017-10-10 22:46:42 -0400

    figured out font-awesome and adding to <head>

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
 
 import '../styles/about.scss'
 
 const About = () => (
   <div className="page-goes-here">
+    <Helmet>
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
+    </Helmet>
     <div className="textArea">
       <p>
         Ear Camera is a collective of bodies creating cochlear art. We are a capillary community, a neural network, an ear that listens, a camerata of peers.
@@ -37,7 +41,15 @@ const About = () => (
         <a target="_blank" href="https://www.soundcloud.com/keithstratton">Keith Stratton</a>
       </p>
       <p>
-        Follow ear camera on <a target="_blank" href="https://www.instagram.com/ear_camera">Instagram</a>.
+        <a target="_blank" href="https://www.instagram.com/ear_camera">
+          <i className="fa fa-instagram" aria-hidden="true">
+          </i>
+        </a>
+        <span className="faSlash"> // </span>
+        <a target="_blank" href="https://twitter.com/earCamera">
+          <i className="fa fa-twitter" aria-hidden="true">
+          </i>
+        </a>
       </p>
     </div>
     <iframe className="youtube" width="560" height="315" src="https://www.youtube.com/embed/Ke0VSWKGJuc" frameborder="0" allowfullscreen></iframe>
