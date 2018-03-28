466406c443b110e94271065a2859eddf69630bc6
michael <mmansourati@gmail.com>
2018-03-27 20:10:28 -0400

    reverted to original landing page method

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,33 +11,13 @@ import '../styles/normalize.css'
 
 export default class IndexPage extends React.Component {
 
-constructor(props) {
-  super(props);
-  this.state = { width: 0, height: "100vh" };
-  this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-}
-
-componentDidMount() {
-  this.updateWindowDimensions();
-  window.addEventListener('resize', this.updateWindowDimensions);
-}
-
-componentWillUnmount() {
-  window.removeEventListener('resize', this.updateWindowDimensions);
-}
-
-updateWindowDimensions() {
-  this.setState({ width: window.innerWidth, height: window.innerHeight });
-  this.splash.style.height = (this.state.height + "px");
-}
-
   render(){
     return(
       <div>
       <Helmet>
           <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
         </Helmet>
-        <div id="splash" ref={(splash) => {this.splash = splash}}>
+        <div id="splash">
           <p id="down-arrow">▾</p>
         </div>
         <div className="contentArea">
